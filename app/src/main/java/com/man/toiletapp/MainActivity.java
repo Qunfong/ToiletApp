547341853aeaaf0466b4958dad2f0c5a9c80b373
Qunfong <qunfong@live.nl>
2019-03-12 18:04:01 +0100

    initial commit

---
+++ app/src/main/java/com/man/toiletapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.man.toiletapp;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        LinearLayout layout = findViewById(R.id.content_session);
+
+//        RecyclerView recyclerView = findViewById(R.id.recyclerview_session);
+//        LinearLayoutManager llm = new LinearLayoutManager(this);
+//        llm.setOrientation(RecyclerView.HORIZONTAL);
+//        recyclerView.setLayoutManager(llm);
+
+        populateSessions(layout);
+    }
+
+    private void populateSessions(LinearLayout layout) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
